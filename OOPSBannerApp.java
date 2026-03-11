ef7004f956c48370881467fb95e21bc3bd1502bc
tg7146 <tg7146@srmist.edu.in>
2026-03-11 13:54:45 +0530

    uc2

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -4,5 +4,13 @@ public class OOPSBannerApp
 	public static void main(String args[])
 	{
 		System.out.println("OOPS");
+		System.out.println("   ***       ***    ******      *****");
+        System.out.println(" **   **   **   **  **    **   **   **");
+        System.out.println("**     ** **     ** **     ** **     ");
+        System.out.println("**     ** **     ** **    **    ***  ");
+        System.out.println("**     ** **     ** ******        *** ");
+        System.out.println("**     ** **     ** **               ** ");
+        System.out.println(" **   **   **   **  **         **   **  ");
+        System.out.println("   ***       ***    **          *****    ");
 	}
 }
