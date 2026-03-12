5523f180eec3ffc2737b5fdf7309c63650337046
tg7146 <tg7146@srmist.edu.in>
2026-03-12 23:36:50 +0530

    UC3

---
+++ OOPSBannerApp3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class OOPSBannerApp3{
+	public static void main(String[] args) {
+
+		System.out.println(String.join("", "   ***       ***    ******      *****"));
+	    System.out.println(String.join("", " **   **   **   **  **    **   **   **"));
+	    System.out.println(String.join("", "**     ** **     ** **     ** **     "));
+	    System.out.println(String.join("", "**     ** **     ** **    **    ***  "));
+	    System.out.println(String.join("", "**     ** **     ** ******        *** "));
+	    System.out.println(String.join("", "**     ** **     ** **               **"));
+	    System.out.println(String.join("", " **   **   **   **  **         **   **"));
+	    System.out.println(String.join("", "   ***       ***    **          *****"));
+
+	}
+}
