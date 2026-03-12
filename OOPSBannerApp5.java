1c6be8f9e89b74f3ac5803d909fb68a7e391d38b
tg7146 <tg7146@srmist.edu.in>
2026-03-13 00:57:37 +0530

    UC5

---
+++ OOPSBannerApp5.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class OOPSBannerApp5 {
+    public static void main(String[] args) {
+
+        String[] bannerLines = {
+            String.join("", "   ***       ***    ******      *****"),
+            String.join("", " **   **   **   **  **    **   **   **"),
+            String.join("", "**     ** **     ** **     ** **     "),
+            String.join("", "**     ** **     ** **    **    ***  "),
+            String.join("", "**     ** **     ** ******        ***"),
+            String.join("", "**     ** **     ** **               **"),
+            String.join("", " **   **   **   **  **         **   **"),
+            String.join("", "   ***       ***    **          *****")
+        };
+
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
+    }
+}
